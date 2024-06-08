70f5b1458bd1ec2dce8109646600af4a8d7fb19b
Undefined-dev74 <aman.kumar.jha953@gmail.com>
2024-06-09 00:15:55 +0530

    chore : change in validation schema

--- src/validations/transaction.validation.ts
+++ src/validations/transaction.validation.ts
@@ -3,7 +3,7 @@ import Joi from 'joi';
 const createTransaction = {
   body: Joi.object().keys({
     investmentId: Joi.number().required(),
-    transactionType: Joi.string().valid('DEPOSIT', 'WITHDRAWAL', 'DIVIDEND').required(),
+    transactionType: Joi.string().valid('DEPOSIT', 'WITHDRAWAL', 'DIVIDEND', 'CREDIT').required(),
     amount: Joi.number().required(),
     status: Joi.string().valid('PENDING', 'SUCCESSFUL', 'FAILED').required(),
     transactionDate: Joi.date().required()
@@ -14,7 +14,7 @@ const getTransactions = {
   query: Joi.object().keys({
     userId: Joi.number(),
     amount: Joi.number(),
-    transactionType: Joi.string().valid('DEPOSIT', 'WITHDRAWAL', 'DIVIDEND'),
+    transactionType: Joi.string().valid('DEPOSIT', 'WITHDRAWAL', 'DIVIDEND', 'CREDIT'),
     status: Joi.string().valid('PENDING', 'SUCCESSFUL', 'FAILED'),
     sortBy: Joi.string(),
     limit: Joi.number().integer(),
